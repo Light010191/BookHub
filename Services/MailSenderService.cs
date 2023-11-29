06b2c579a16aed19b4b26784deb7bb31ee669224
Light010191 <99544543+Light010191@users.noreply.github.com>
2023-11-30 00:47:36 +0300

    Update MailSenderService.cs

--- Services/MailSenderService.cs
+++ Services/MailSenderService.cs
@@ -7,15 +7,15 @@ namespace BooksHub.Services
 			public async Task Send(string email, string subject, string content)
 			{
 				var emailMessage = new MimeMessage();
-				emailMessage.From.Add(new MailboxAddress("EShop", "postmaster@sandboxe2a0e6699dd74fd99df559ca2398a76a.mailgun.org"));
+				emailMessage.From.Add(new MailboxAddress("EShop", "postmaster@....mailgun.org"));
 				emailMessage.To.Add(new MailboxAddress("Client", email));
 				emailMessage.Subject = subject;
 				emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = content };
 				using (var client = new MailKit.Net.Smtp.SmtpClient())
 				{
 					await client.ConnectAsync("smtp.mailgun.org", 587, false);
-					await client.AuthenticateAsync("postmaster@sandboxe2a0e6699dd74fd99df559ca2398a76a.mailgun.org",
-                        "bcb97a00f152ad2dc78825b809a796e4-1c7e8847-46088acd");
+					await client.AuthenticateAsync("postmaster@....mailgun.org",
+                        "key");
 					await client.SendAsync(emailMessage);
 					await client.DisconnectAsync(true);
 				}
